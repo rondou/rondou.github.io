1f6b1bed2d14a1b1df96165038918e888adca8d1
Rondou Chen <40and44sis@gmail.com>
2016-01-25 11:52:02 +0800

    test js

---
+++ test.js
@@ -0,0 +1,8 @@
+document.write("hello".slice(1, 3) +  "<br / >");
+var v1 = "hello";
+document.write(v1.slice(1, 3) +  "<br / >");
+document.write(33.33633.toFixed(2) + "<br / >");
+var v2 = 33.33633;
+document.write(v2.toFixed(2) + "<br / >");
+var v3;
+document.write(typeof(v3));
